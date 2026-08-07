693a36c92df50e58d9bd014c44a226e304288eb1
andreydecorvital-sudo <andreydecorvital@gmail.com>
2026-08-07 17:13:47 -0300

    feat: add src/vitalimagens/__init__.py

---
+++ src/vitalimagens/__init__.py
@@ -0,0 +1,6 @@
+"""Vital Imagens: composição comercial determinística e auditável."""
+
+from .cover import CoverRenderer, CoverSpec, load_preset
+
+__all__ = ["CoverRenderer", "CoverSpec", "load_preset"]
+
